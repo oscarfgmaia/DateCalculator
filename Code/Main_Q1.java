d072a76716e097dc5f872c216a261af59785f349
oscarfgmaia <oscarfgmaia@gmail.com>
2020-06-29 13:27:30 -0300

    Update Main_Q1.java
    Retirado o package e linhas extras

--- Code/Main_Q1.java
+++ Code/Main_Q1.java
@@ -1,11 +1,8 @@
-package trabalho;
-
 public class Main_Q1 {
 
 	public static void main(String[] args) {
-		Datas_Q1 date = new Datas_Q1(31, 4, 2020);
+		Datas_Q1 date = new Datas_Q1(12, 1, 2020);
 		System.out.println(date);
-
 	}
 
 }
